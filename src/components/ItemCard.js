5b8484d406ef104b7bcdd2a7bfaad6b4c3aae6ed
William Gotterer <william.gotterer@gmail.com>
2022-03-10 20:04:02 -0500

    styling flex box

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,14 +1,16 @@
 import React from "react";
-import "../styling/ItemCard.css"
+import "../styling/ItemCard.css";
 
 function ItemCard(props) {
   return (
     <div className="item-container">
-      <button className="item-button">Buy now</button>
       <div className="info-container">
         <div className="item-title">{props.title}</div>
         <div className="item-description">{props.description}</div>
-        <div className="item-price">{props.price}</div>
+        <div className="price-button-container">
+          <div className="item-price">{props.price}</div>
+          <button className="item-button">Buy now</button>
+        </div>
       </div>
       <img
         className="item-pic"
